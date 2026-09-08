d934ba26c760733e50c52fa591973f54cb1dc052
Wilson Andrade <wilson.a.andrade01@gmail.com>
2026-09-08 17:39:42 +0200

    commit para solucionar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://wilsonaandrade01-beep.github.io',
+  base: '/minor-mass', // porque tu repo se llama "beep"
+});
